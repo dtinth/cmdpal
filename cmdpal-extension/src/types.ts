caf3841fcb2a02ae8d391e9d7bd3b380db943af8
dtinth on MBP M1 <dtinth@spacet.me>
2021-11-06 15:37:05 +0700

    Add docs

--- cmdpal-extension/src/types.ts
+++ cmdpal-extension/src/types.ts
@@ -1,14 +1,41 @@
 export type CmdpalEvent = {
   detail: {
+    /**
+     * Fired by cmdpal when the command palette is opened.
+     */
+    open?: {}
+
+    /**
+     * Fired by the page to register commands in the command palette.
+     * This only works when the command palette is open.
+     */
     register?: {
+      /**
+       * The command group. This will replace existing commands in the same group.
+       */
+      group: string
+
+      /**
+       * The commands to register.
+       */
       commands: {
+        /** The command ID */
         id: string
+
+        /** The searchable text for the command. */
         title: string
+
+        /** Extra text that is displayed less prominently next to the command. */
         description?: string
+
+        /** Extra text that is displayed below the command. */
         detail?: string
       }[]
-      group: string
     }
+
+    /**
+     * Fired by cmdpal when a command will be executed.
+     */
     execute?: {
       command: string
     }
